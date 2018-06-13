901006485d2298fa19c9d7160b0e520eb912a215
Charles Rothbacher <rothbacherc@gmail.com>
2018-06-12 17:39:01 -0700

    fjd

--- googleOauth2.ts
+++ googleOauth2.ts
@@ -1,6 +1,6 @@
 class googleOauth2 {
 static id: string = '678785630258-0ci1lra9jogq2keu7rgoajiq37eaagiv.apps.googleusercontent.com/';
-static secret:string = 'M9DU0zYAmUDXTtDgNLJwAOxl';
+static secret: string = 'M9DU0zYAmUDXTtDgNLJwAOxl';
 
 }
 export default googleOauth2;
