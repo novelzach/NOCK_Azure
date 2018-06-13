54a1faa30aea2b8a71ff232a923b51bd503dc17f
Charles Rothbacher <rothbacherc@gmail.com>
2018-06-12 17:34:05 -0700

    jdsk

--- googleOauth2.ts
+++ googleOauth2.ts
@@ -1,5 +1,5 @@
 class googleOauth2 {
-static id: string = 'http://678785630258-0ci1lra9jogq2keu7rgoajiq37eaagiv.apps.googleusercontent.com/';
+static id: string = '678785630258-0ci1lra9jogq2keu7rgoajiq37eaagiv.apps.googleusercontent.com/';
 static secret:string = 'M9DU0zYAmUDXTtDgNLJwAOxl';
 
 }
